305550dd3656dc544945e3a0d02bac3cdc3dc0dd
JeBear76 <jerepondumie@gmail.com>
2019-08-31 12:26:04 +0200

    more dumb errors

--- robot_controller.py
+++ robot_controller.py
@@ -5,21 +5,19 @@ import pigpio
 import time
 
 class Robot_Controller(object):
-    BOARD2BCM=[
-   -1, -1,  2, -1,  3, -1,  4, 14,
-   -1, 15, 17, 18, 27, -1, 22, 23,
-   -1, 24, 10, -1,  9, 25, 11,  8,
-   -1,  7,  0,  1,  5, -1,  6, 12,
-   13, -1, 19, 16, 26, 20, -1, 21]
-
     def Board_to_BCM(self, pin):
-        global BOARD2BCM
         if pin < 1 or pin > 40:
             return -1
         else:
             return BOARD2BCM[pin-1]
         
-    def __init__(self, camera):
+    def __init__(self, camera):        
+        self.BOARD2BCM=[-1, -1,  2, -1,  3, -1,  4, 14,
+                       -1, 15, 17, 18, 27, -1, 22, 23,
+                       -1, 24, 10, -1,  9, 25, 11,  8,
+                       -1,  7,  0,  1,  5, -1,  6, 12,
+                       13, -1, 19, 16, 26, 20, -1, 21]
+        
         self.usePigPio = False
         self.camera = camera
         self.threadActive = True
